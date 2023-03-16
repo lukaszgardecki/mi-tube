04099763aca9eb83f28a7612f048c5d4e0670dc0
lukaszgardecki <105553857+lukaszgardecki@users.noreply.github.com>
2023-03-16 22:57:28 +0100

    PR - create genre list page (#12)
    * Add method to search all genres
    
    * Add genre-list page
     Add GET method with /gatunki-filmowe endpoint in GenreController.
     Create an html template displays a list of movie genres.
     Add genres list link in layout.html.

--- src/main/java/com/example/app/domain/genre/GenreService.java
+++ src/main/java/com/example/app/domain/genre/GenreService.java
@@ -3,7 +3,9 @@ package com.example.app.domain.genre;
 import com.example.app.domain.genre.genre.GenreDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @Service
 public class GenreService {
@@ -16,5 +18,11 @@ public class GenreService {
     public Optional<GenreDto> findGenreByName(String name) {
         return genreRepository.findByNameIgnoreCase(name)
                 .map(GenreMapper::map);
+    }
+
+    public List<GenreDto> findAllGenres() {
+        return StreamSupport.stream(genreRepository.findAll().spliterator(), false)
+                .map(GenreMapper::map)
+                .toList();
     }
 }

6c4bd745cabbc6e73c82bd80fb929c4f7af4ea93
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-03-16 21:41:29 +0100

    Add description column to genre table

--- src/main/java/com/example/app/domain/genre/Genre.java
+++ src/main/java/com/example/app/domain/genre/Genre.java
@@ -11,6 +11,7 @@ public class Genre {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
 
     public Long getId() {
         return id;
@@ -26,5 +27,13 @@ public class Genre {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }

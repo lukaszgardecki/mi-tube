24045d28458373800dd20a630490b005528d0191
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-03-16 22:49:20 +0100

    Add genre-list page
     Add GET method with /gatunki-filmowe endpoint in GenreController.
     Create an html template displays a list of movie genres.
     Add genres list link in layout.html.

--- src/main/java/com/example/app/web/GenreController.java
+++ src/main/java/com/example/app/web/GenreController.java
@@ -33,4 +33,11 @@ public class GenreController {
         model.addAttribute("movies", movies);
         return "movie-listing";
     }
+
+    @GetMapping("/gatunki-filmowe")
+    public String getGenreList(Model model) {
+        List<GenreDto> genres = genreService.findAllGenres();
+        model.addAttribute("genres", genres);
+        return "genre-listing";
+    }
 }

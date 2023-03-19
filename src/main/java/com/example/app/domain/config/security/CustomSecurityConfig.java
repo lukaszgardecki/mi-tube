81411049869fffdfcad6fb22b9c4075fefb22972
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-03-19 14:31:02 +0100

    Add basic security configuration

---
+++ src/main/java/com/example/app/domain/config/security/CustomSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.app.domain.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class CustomSecurityConfig {
+    private static final String USER_ROLE = "USER";
+    private static final String EDITOR_ROLE = "EDITOR";
+    private static final String ADMIN_ROLE = "ADMIN";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                "/img/**",
+                "/scripts/**",
+                "/styles/**"
+        );
+    }
+}

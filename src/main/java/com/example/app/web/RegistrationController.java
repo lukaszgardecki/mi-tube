e8bff3ce213fa5fa8a69a216e4f594d068457160
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-19 20:19:31 +0200

    Create registration controller

---
+++ src/main/java/com/example/app/web/RegistrationController.java
@@ -0,0 +1,30 @@
+package com.example.app.web;
+
+import com.example.app.domain.user.UserService;
+import com.example.app.domain.user.dto.UserRegistrationDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistrationController {
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/rejestracja")
+    public String getRegistrationForm(Model model) {
+        UserRegistrationDto userRegistration = new UserRegistrationDto();
+        model.addAttribute("user", userRegistration);
+        return "registration-form";
+    }
+
+    @PostMapping("/rejestracja")
+    public String register(UserRegistrationDto userRegistration) {
+        userService.registerUserWithDefaultRole(userRegistration);
+        return "redirect:/";
+    }
+}

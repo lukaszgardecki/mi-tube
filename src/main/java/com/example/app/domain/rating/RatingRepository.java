29251f58f07e00b26f831618f32b0cc926bdd5de
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-20 16:26:05 +0200

    Create rating repository

---
+++ src/main/java/com/example/app/domain/rating/RatingRepository.java
@@ -0,0 +1,10 @@
+package com.example.app.domain.rating;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RatingRepository extends CrudRepository<Rating, Long> {
+
+    Optional<Rating> findByUser_EmailAndMovie_Id(String userEmail, Long movieId);
+}

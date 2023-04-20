254b4536f117ada84e32737f99c3b97b5b1445d3
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-04-20 17:27:45 +0200

    Update MovieController - displaying vote stars

--- src/main/java/com/example/app/web/MovieController.java
+++ src/main/java/com/example/app/web/MovieController.java
@@ -2,7 +2,9 @@ package com.example.app.web;
 
 import com.example.app.domain.movie.MovieService;
 import com.example.app.domain.movie.dto.MovieDto;
+import com.example.app.domain.rating.RatingService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +14,25 @@ import org.springframework.web.server.ResponseStatusException;
 @Controller
 public class MovieController {
     private final MovieService movieService;
+    private final RatingService ratingService;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, RatingService ratingService) {
         this.movieService = movieService;
+        this.ratingService = ratingService;
     }
 
     @GetMapping("/film/{id}")
-    public String getMovie(@PathVariable long id, Model model) {
+    public String getMovie(@PathVariable long id, Model model, Authentication authentication) {
         MovieDto movie = movieService.findMovieById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         model.addAttribute("movie", movie);
+
+        if (authentication != null) {
+            String currentUserEmail = authentication.getName();
+            Integer rating = ratingService.getUserRatingForMovie(currentUserEmail, id).orElse(0);
+            model.addAttribute("userRating", rating);
+        }
         return "movie";
     }
 }
